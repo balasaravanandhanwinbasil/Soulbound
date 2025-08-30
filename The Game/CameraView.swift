ca8c92a06691869ee4d3327f647f5352d81da26c
JH-coder231 <t20537179@gmail.com>
2025-08-30 16:48:31 +0800

    Changed the camera view

--- The Game/CameraView.swift
+++ The Game/CameraView.swift
@@ -99,10 +99,27 @@ extension UIDevice {
 }
 
 
-struct CameraView: UIViewControllerRepresentable {
-    func makeUIViewController(context: Context) -> some UIViewController {
-        return ViewController()
+struct CameraView: UIViewRepresentable {
+    
+    let session: AVCaptureSession
+    
+    func makeUIView(context: Context) -> UIView {
+        let view = UIView(frame: .zero)
+        let previewLayer = AVCaptureVideoPreviewLayer(session: session)
+        
+        previewLayer.videoGravity = .resizeAspectFill
+        previewLayer.frame = view.bounds
+        previewLayer.connection?.videoRotationAngle = 0
+        view.layer.addSublayer(previewLayer)
+        
+        return view
     }
-
-    func updateUIViewController(_ uiViewController: UIViewControllerType, context: Context) {}
+    
+    func updateUIView(_ uiView: UIView, context: Context) {
+        Task {
+            if let previewLayer = uiView.layer.sublayers?.first as? AVCaptureVideoPreviewLayer {
+                previewLayer.frame = uiView.bounds
+            }
+        }
+    }
 }
